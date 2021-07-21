37c2c47539dfcad2f01979fbe26805ced4144597
dungeonsnd <dungeonsnd@gmail.com>
2021-07-21 17:52:32 +0800

    postjosn

--- net/http/client.go
+++ net/http/client.go
@@ -7,7 +7,40 @@ import (
 	"net/http"
 )
 
-func PostJson(url string, parms interface{}) (interface{}, error) {
+func PostJson(url string, parms interface{}, ret interface{}) error {
+	client := &http.Client{}
+	data, err := json.Marshal(parms)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	// req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &ret)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PostJsonReturnMap(url string, parms interface{}) (interface{}, error) {
 	client := &http.Client{}
 	data, err := json.Marshal(parms)
 	if err != nil {
