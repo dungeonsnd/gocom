e3b55ec151ecc11b2527a0763d685da8621e185d
wenchao <wenchao@iscas.ac.cn>
2021-10-16 22:25:32 +0800

    add pkcs5 for aes

--- encrypt/aes/aes.go
+++ encrypt/aes/aes.go
@@ -5,20 +5,30 @@ import (
 	"crypto/cipher"
 
 	"github.com/dungeonsnd/gocom/encrypt/encoding"
-	pkcs7 "github.com/dungeonsnd/gocom/encrypt/padding"
+	pkcs "github.com/dungeonsnd/gocom/encrypt/padding"
 )
 
-func AesDecryptForB64(origDataB64 string, key []byte, iv []byte) ([]byte, error) {
+func AesDecryptForB64ByPkcs5Padding(origDataB64 string, key []byte, iv []byte) ([]byte, error) {
 
 	p, err := encoding.Base64Decode(origDataB64)
 	if err != nil {
 		return nil, err
 	}
 
-	return AesDecrypt(p, key, iv)
+	return AesDecryptByPkcs5Padding(p, key, iv)
 }
 
-func AesDecryptForB64AndHexKey(origDataB64 string, keyHex string, ivHex string) ([]byte, error) {
+func AesDecryptForB64ByPkcs7Padding(origDataB64 string, key []byte, iv []byte) ([]byte, error) {
+
+	p, err := encoding.Base64Decode(origDataB64)
+	if err != nil {
+		return nil, err
+	}
+
+	return AesDecryptByPkcs7Padding(p, key, iv)
+}
+
+func AesDecryptForB64AndHexKeyByPkcs5Padding(origDataB64 string, keyHex string, ivHex string) ([]byte, error) {
 	k, err := encoding.HexDecode(keyHex)
 	if err != nil {
 		return nil, err
@@ -33,10 +43,10 @@ func AesDecryptForB64AndHexKey(origDataB64 string, keyHex string, ivHex string) 
 		return nil, err
 	}
 
-	return AesDecrypt(p, k, i)
+	return AesDecryptByPkcs5Padding(p, k, i)
 }
 
-func AesEncryptForHexKey(origData []byte, keyHex string, ivHex string) ([]byte, error) {
+func AesDecryptForB64AndHexKeyByPkcsP7adding(origDataB64 string, keyHex string, ivHex string) ([]byte, error) {
 	k, err := encoding.HexDecode(keyHex)
 	if err != nil {
 		return nil, err
@@ -45,23 +55,66 @@ func AesEncryptForHexKey(origData []byte, keyHex string, ivHex string) ([]byte, 
 	if err != nil {
 		return nil, err
 	}
-	return AesEncrypt(origData, k, i)
+
+	p, err := encoding.Base64Decode(origDataB64)
+	if err != nil {
+		return nil, err
+	}
+
+	return AesDecryptByPkcs7Padding(p, k, i)
 }
 
-func AesEncrypt(origData []byte, key []byte, iv []byte) ([]byte, error) {
+func AesEncryptForHexKeyByPkcs5Padding(origData []byte, keyHex string, ivHex string) ([]byte, error) {
+	k, err := encoding.HexDecode(keyHex)
+	if err != nil {
+		return nil, err
+	}
+	i, err := encoding.HexDecode(ivHex)
+	if err != nil {
+		return nil, err
+	}
+	return AesEncryptByPkcs5Padding(origData, k, i)
+}
+
+func AesEncryptForHexKeyByPkcs7Padding(origData []byte, keyHex string, ivHex string) ([]byte, error) {
+	k, err := encoding.HexDecode(keyHex)
+	if err != nil {
+		return nil, err
+	}
+	i, err := encoding.HexDecode(ivHex)
+	if err != nil {
+		return nil, err
+	}
+	return AesEncryptByPkcs7Padding(origData, k, i)
+}
+
+func AesEncryptByPkcs5Padding(origData []byte, key []byte, iv []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	blockSize := block.BlockSize()
-	origData = pkcs7.Padding(origData, blockSize)
+	origData = pkcs.Pkcs5Padding(origData, blockSize)
 	blockMode := cipher.NewCBCEncrypter(block, iv)
 	crypted := make([]byte, len(origData))
 	blockMode.CryptBlocks(crypted, origData)
 	return crypted, nil
 }
 
-func AesDecrypt(crypted []byte, key []byte, iv []byte) ([]byte, error) {
+func AesEncryptByPkcs7Padding(origData []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData = pkcs.Pkcs7Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+func AesDecryptByPkcs5Padding(crypted []byte, key []byte, iv []byte) ([]byte, error) {
 	// fmt.Printf("AesDecrypt, crypted=%v, key=%v, iv=%v\n", string(crypted), string(key), string(iv))
 
 	block, err := aes.NewCipher(key)
@@ -72,7 +125,25 @@ func AesDecrypt(crypted []byte, key []byte, iv []byte) ([]byte, error) {
 	blockMode := cipher.NewCBCDecrypter(block, iv)
 	origData := make([]byte, len(crypted))
 	blockMode.CryptBlocks(origData, crypted)
-	origData, err = pkcs7.Unpadding(origData)
+	origData, err = pkcs.Pkcs5Unpadding(origData)
+	if err != nil {
+		return nil, err
+	}
+	return origData, nil
+}
+
+func AesDecryptByPkcs7Padding(crypted []byte, key []byte, iv []byte) ([]byte, error) {
+	// fmt.Printf("AesDecrypt, crypted=%v, key=%v, iv=%v\n", string(crypted), string(key), string(iv))
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData, err = pkcs.Pkcs7Unpadding(origData)
 	if err != nil {
 		return nil, err
 	}
