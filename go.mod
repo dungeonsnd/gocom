633b1ca5e27833e942a88b00216bfd3566ebf9c2
wenchao <wenchao@iscas.ac.cn>
2021-11-13 05:43:12 +0800

    restore log bcz of filename formatter

--- go.mod
+++ go.mod
@@ -4,15 +4,18 @@ go 1.14
 
 require (
 	github.com/anthonynsimon/bild v0.11.1
-	github.com/dungeonsnd/bluetooth v0.3.4
 	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/onrik/logrus v0.9.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/tidwall/gjson v1.11.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- test_main.go
+++ test_main.go
@@ -1,24 +1,26 @@
+/*
+ * @Author: wenchao
+ * @Date: 2021-11-01 12:19:10
+ * @LastEditors: wenchao
+ * @LastEditTime: 2021-11-13 05:11:49
+ * @Description:
+ */
 package main
 
 import (
-	"github.com/dungeonsnd/gocom/bluetoothutil"
-	"github.com/dungeonsnd/gocom/test/encrypt/aes"
-	"github.com/dungeonsnd/gocom/test/encrypt/rsa"
-
-	// "tinygo.org/x/bluetooth"
-	"github.com/dungeonsnd/bluetooth"
+	"github.com/dungeonsnd/gocom/log4go"
 )
 
+// "tinygo.org/x/bluetooth"
+
 func main() {
-	aes.Run()
-	rsa.Run()
+	// aes.Run()
+	// rsa.Run()
 	// thumbnail.ResizeImage("", "", 0)
 
-	// log4go.InitLog(".", "test", 0, 1000, 10485760, 100)
-	// log4go.SetLogLevel(5) // InfoLevel=4, DebugLevel=5, logrus.TraceLevel=6
-	// log4go.I("================[%v Started]================\n", os.Args[0])
-
-	bluetoothutil.StartByServiceUUID("1", "2", nil,
-		bluetooth.CharacteristicWritePermission,
-		bluetooth.CharacteristicNotifyPermission|bluetooth.CharacteristicReadPermission)
+	log4go.InitLog(".", "test", 0, 1000, 10485760, 100)
+	log4go.SetLogLevel(5) // InfoLevel=4, DebugLevel=5, logrus.TraceLevel=6
+	// log4go.I("================[%v Started]================\n\n", os.Args[0])
+	log4go.D("aaa\n\n")
+	log4go.D("bbb")
 }
